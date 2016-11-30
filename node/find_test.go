cfc70bb63ec0d5e4fc003a72637988d1cc3748c9
Douglas Hubler <douglas@hubler.us>
2016-11-30 15:41:49 -0500

    remove old code

--- node/find_test.go
+++ node/find_test.go
@@ -2,6 +2,8 @@ package node
 
 import (
 	"testing"
+
+	"github.com/c2stack/c2g/meta"
 )
 
 func TestFindPathSlice(t *testing.T) {
@@ -108,4 +110,85 @@ func TestFindPathSlice(t *testing.T) {
 			}
 		}
 	}
+}
+
+const walkTestModule = `
+module food {
+	prefix "x";
+	namespace "y";
+	revision 0000-00-00 {
+		description "";
+	}
+	list fruits  {
+		key "name";
+		leaf name {
+			type string;
+		}
+		container origin {
+			leaf country {
+				type string;
+			}
+		}
+		choice shipment {
+			case water {
+				container boat {
+					leaf name {
+						type string;
+					}
+				}
+			}
+			case air {
+				container plane {
+					leaf name {
+						type string;
+					}
+				}
+			}
+		}
+	}
+}
+`
+
+func TestFindPathIntoListItemContainer(t *testing.T) {
+	m, root := LoadPathTestData()
+	tests := []string{
+		"fruits=apple/origin",
+		"fruits=apple/boat",
+	}
+	for _, test := range tests {
+		target := NewBrowser(m, MapNode(root)).Root().Find(test)
+		if target.LastErr != nil {
+			t.Fatal(target.LastErr)
+		} else if target.IsNil() {
+			t.Fatal("Could not find target " + test)
+		}
+	}
+}
+
+func LoadPathTestData() (*meta.Module, map[string]interface{}) {
+	// avoid using json to load because that needs edit/INSERT and
+	// we don't want to use code to load seed data that we're trying to test
+	data := map[string]interface{}{
+		"fruits": []map[string]interface{}{
+			map[string]interface{}{
+				"name": "banana",
+				"origin": map[string]interface{}{
+					"country": "Brazil",
+				},
+				"plane": map[string]interface{}{
+					"name": "747c",
+				},
+			},
+			map[string]interface{}{
+				"name": "apple",
+				"origin": map[string]interface{}{
+					"country": "US",
+				},
+				"boat": map[string]interface{}{
+					"name": "SS Hudson",
+				},
+			},
+		},
+	}
+	return YangFromString(walkTestModule), data
 }
