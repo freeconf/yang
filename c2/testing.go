d0d28ddc750435731d72e72c3b3f3437ae17f9ca
Douglas Hubler <douglas@hubler.us>
2017-10-16 13:04:51 -0400

    fix build, require 1.9 to use testing.Helper method

--- c2/testing.go
+++ c2/testing.go
@@ -53,6 +53,7 @@ func Diff(t *testing.T, a []byte, b string) bool {
 // DiffFiles will compare two files and emit formatted difference
 // useful in "Golden File Testing", return true if no differences
 func DiffFiles(t *testing.T, a string, b string) bool {
+	t.Helper()
 	cmd := exec.Command("diff", "-U", "3", a, b)
 	var outBuff bytes.Buffer
 	cmd.Stdout = &outBuff
@@ -68,6 +69,7 @@ func DiffFiles(t *testing.T, a string, b string) bool {
 // is passed, then it replaces contents of file on disk. This testing strategy
 // if known as "gold files" and can be found in many projects including the Go SDK
 func Gold(t *testing.T, update bool, actual []byte, gfile string) bool {
+	t.Helper()
 	if update {
 		if err := ioutil.WriteFile(gfile, actual, 0666); err != nil {
 			panic(err)
