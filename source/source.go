9dbdc8a2ce0f52f98e53cc969ed1038a7954812f
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2023-05-24 08:04:04 +0200

    Add yang source for embedded directories

--- source/source.go
+++ source/source.go
@@ -1,9 +1,12 @@
 package source
 
 import (
+	"bytes"
+	"embed"
 	"fmt"
 	"io"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -53,5 +56,34 @@ func Dir(root string) Opener {
 			return nil, nil
 		}
 		return stream, err
+	}
+}
+
+// EmbedDir will find a yang file in an embedded file system
+// because Embed doesn't support symlinks, EmbedDir also looks for version specific files of yang
+// with a pattern of <resourceId>@<version><ext>, e.g. ietf-inet-types@2013-07-15.yang
+func EmbedDir(fs embed.FS, root string) Opener {
+	return func(resourceId string, ext string) (io.Reader, error) {
+		name := fmt.Sprint(resourceId, ext)
+		bin, err := fs.ReadFile(path.Join(root, name))
+		if err == nil {
+			return bytes.NewReader(bin), nil
+		}
+
+		entries, err := fs.ReadDir(root)
+		if err != nil {
+			return nil, nil
+		}
+
+		for _, e := range entries {
+			if strings.HasPrefix(e.Name(), resourceId+"@") && strings.HasSuffix(e.Name(), ext) {
+				bin, err = fs.ReadFile(path.Join(root, e.Name()))
+				if err != nil {
+					return nil, err
+				}
+				return bytes.NewReader(bin), nil
+			}
+		}
+		return nil, nil
 	}
 }
