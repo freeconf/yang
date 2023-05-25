ef1bdfe4106ef17628246b3f4ece7056e73ba30f
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2023-05-25 13:05:32 +0200

    Add yang source for embedded directories (#48)
    * Add yang source for embedded directories
    
    * Add testcased for embedded source reader

--- source/source_test.go
+++ source/source_test.go
@@ -1,6 +1,8 @@
 package source
 
 import (
+	"embed"
+	_ "embed"
 	"testing"
 )
 
@@ -12,5 +14,41 @@ func TestDirNotFound(t *testing.T) {
 	}
 	if err != nil {
 		t.Error("expected no err")
+	}
+}
+
+//go:embed testdata/*.yang
+var nothing embed.FS
+
+func TestEmbedDir_Nothing(t *testing.T) {
+	s := EmbedDir(nothing, "testdata")
+	y, err := s("nothing", ".yang")
+	if err != nil {
+		t.Fatalf("expected to find 'nothing.yang', got: %v", err)
+	}
+	if y == nil {
+		t.Fatalf("expected reader")
+	}
+}
+
+func TestEmbedDir_NotFound(t *testing.T) {
+	s := EmbedDir(nothing, "testdata")
+	y, err := s("notfound", ".yang")
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+	if y != nil {
+		t.Fatalf("expected no reader, got: %v", y)
+	}
+}
+
+func TestEmbedDir_SpecialNothing(t *testing.T) {
+	s := EmbedDir(nothing, "testdata")
+	y, err := s("specialnothing", ".yang")
+	if err != nil {
+		t.Fatalf("expected to find 'nothing.yang', got: %v", err)
+	}
+	if y == nil {
+		t.Fatalf("expected reader")
 	}
 }
